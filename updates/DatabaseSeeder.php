e2c5c374ae9ca984814e5e65921ac3aa17405265
Evgeny Epifanov <workeev@gmail.com>
2020-04-01 18:39:48 +0000

    seeder

--- updates/DatabaseSeeder.php
+++ updates/DatabaseSeeder.php
@@ -3,28 +3,45 @@
 namespace EEV\Blog\Updates;
 
 use EEV\Blog\Models\Post;
+use EEV\Blog\Models\PostCategory;
 use October\Rain\Database\Updates\Seeder;
 use Faker;
 
 class DatabaseSeeder extends Seeder
 {
+    private $faker;
+
+    public function __construct()
+    {
+        $this->faker = Faker\Factory::create('ru_RU');
+    }
+
     public function run()
     {
-        $faker = Faker\Factory::create('ru_RU');
+        for($i = 1; $i < 100; $i++) {
+            Post::create($this->getPost($i));
+        }
 
-        Post::insert([
-            [
-                'title' => $faker->realText(50),
-                'content' => $faker->realText(2000),
-                'preview' => $faker->realText(500),
-                'slug' => 'post-1',
-            ],
-            [
-                'title' => $faker->realText(50),
-                'content' => $faker->realText(2000),
-                'preview' => $faker->realText(500),
-                'slug' => 'post-2',
-            ],
-        ]);
+        for($i = 1; $i < 10; $i++) {
+            PostCategory::create($this->getPostCategory($i));
+        }
+    }
+
+    public function getPost($index = null) {
+        return [
+            'title' => $this->faker->realText(50),
+            'content' => $this->faker->realText(2000),
+            'preview' => $this->faker->realText(500),
+            'slug' => 'post-' . ($index ? $index : ''),
+        ];
+    }
+
+    public function getPostCategory($index = null) {
+        return [
+            'title' => $this->faker->realText(20),
+            'content' => $this->faker->realText(2000),
+            'preview' => $this->faker->realText(500),
+            'slug' => 'category-' . ($index ? $index : ''),
+        ];
     }
 }
